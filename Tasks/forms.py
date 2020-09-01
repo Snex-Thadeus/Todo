216bd977678aa8d8b877f3a4dd4622b98774bfb0
Snex-Thadeus <lwande94@gmail.com>
2020-09-01 11:46:13 +0300

    To Do app by python django

---
+++ Tasks/forms.py
@@ -0,0 +1,11 @@
+from django.forms import forms
+from .models import *
+from django import forms
+from django.forms import ModelForm
+
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task...'}))
+    class Meta:
+        model = Task
+        fields = '__all__'
